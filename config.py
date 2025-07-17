c760d5e0363bd908517886b9908110d56904d756
piyushsonker024-hue <piyushsonker024@gmail.com>
2025-07-17 11:42:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 POPPLER_PATH = r"C:\Users\hp\OneDrive\Desktop\chatwithpdf\poppler-24.08.0\Library\bin"
 TESSERACT_PATH = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 PDF_DIR = r"C:\Users\hp\Downloads\zomato bill.pdf"
 OUTPUT_DIR = r"C:\Users\hp\OneDrive\Desktop\pdf_output"
-GEMINI_API_KEY = "AIzaSyB2oGskrrrkk5iv1xeghSIwCDk-qUUE_6I"
+GEMINI_API_KEY =os.getenv("GEMINI_API_KEY") 
